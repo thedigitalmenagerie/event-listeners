742d95277196b797fcc40896d1c83d20727dc346
Honey-Rae Swan <honey-raeswan@Honey-Raes-MacBook-Air.local>
2021-01-26 18:12:30 -0600

    added and checked code from last night

--- main.js
+++ main.js
@@ -1,162 +1,138 @@
-// console.log('connected');// arrays
+console.log('CONNECTED!');
 
-// //objects
+const pies = [
+  {
+    name: 'Dutch Apple Pie',
+    ingredients: 'apples,sugar,butter,nutmeg,dutch people',
+    bakeTemp: 5000,
+    drinkPairing: 'Earl Grey Tea',
+    imageUrl:
+      'https://images-gmi-pmc.edge-generalmills.com/b6a2a4e7-73f5-4aec-9bb6-f2b5054d65e6.jpg',
+    instructor: 'Doc',
+    iceCream: 'Vanilla',
+  },
+  {
+    name: 'Berry Pie',
+    ingredients: 'berries',
+    bakeTemp: 400,
+    drinkPairing: 'wine',
+    imageUrl:
+      'https://tastesbetterfromscratch.com/wp-content/uploads/2015/11/Triple_Berry_Pie8.jpg',
+    instructor: 'Doc',
+    iceCream: 'banana',
+  },
+  {
+    name: 'Pumpkin Pie',
+    ingredients: 'pumpkins, nutmeg, cinnamon, graham crackers, pilgrims',
+    bakeTemp: 42,
+    drinkPairing: 'egg nog',
+    imageUrl:
+      'https://cf-images.us-east-1.prod.boltdns.net/v1/static/1033249144001/22a0c25d-2fee-415c-a4e7-91d008e276bb/a904f3e2-3cd9-40d6-ace9-f8dbd2d616cd/1280x720/match/image.jpg',
+    instructor: 'Aja',
+    iceCream: 'Vanilla',
+  },
+  {
+    name: 'Shoo Fly Pie',
+    ingredients: 'Molasses, nutmeg, cinnamon, butter, graham cracker ',
+    bakeTemp: 1234,
+    drinkPairing: 'Apple Cider',
+    imageUrl:
+      'https://static01.nyt.com/images/2016/09/27/dining/27COOKING-SHOOFLY-PIE2/27COOKING-SHOOFLY-PIE2-articleLarge.jpg',
+    instructor: 'Aja',
+    iceCream: 'Coffee',
+  },
+  {
+    name: 'Pecan Pie',
+    ingredients: 'Pecans, sugar, butter, flour',
+    bakeTemp: 5000,
+    drinkPairing: 'Milk',
+    imageUrl:
+      'https://cookiesandcups.com/wp-content/uploads/2018/10/pecanpie-3.jpg',
+    instructor: 'Trinity',
+    iceCream: 'Vanilla',
+  },
+  {
+    name: 'Keylime Pie',
+    ingredients: 'lemons, sugar, butter, flour',
+    bakeTemp: 5000,
+    drinkPairing: 'Water',
+    imageUrl:
+      'https://www.browneyedbaker.com/wp-content/uploads/2012/05/key-lime-pie-2-1200.jpg',
+    instructor: 'Trinity',
+    iceCream: 'none',
+  },
+];
 
-// // curly brackets key value pairs which must be seperated by a comma make an object 
-// // can be empty
-
-// const firstObj = {};
-
-// const user = {
-//   username: 'biz.markie',
-//   password: 'abc123',
-//   lovesJavaScript: true,
-//   favoriteNumber: 42, // trailing comma is optional some places require for last element in an object
-// }
-
-// // access elements in an object with ot notation
-
-// console.log(user.lovesJavascript);
-
-// console.log(user.username);
-
-// // bracket notation with dot notation or bracket notation but that must be a string inside the brackets
-
-// console.log(user['password']);
-// console.log(user['favoriteNumber']);
-// console.log(user.favoriteNumber);
-
-// //assigning values after object is defined/ with dot or bracket notation
-
-// const newUser = {
-//   isNew: true,
-// }
-
-// newUser.userName = 'fresh.prince'; // dot
-// newUser['password'] = 'abcd1234'; // bracket
-
-// console.log(newUser);  // notice these are logging in alphabetical order so you should code them that way
-
-// methods are functions as values in an object which are mutable data types
-
-//  const newObject = { 
-//    username: 'Honey-Rae',
-//     sayHello: function () { // anonymous function
-//     console.log(`${this.username} says Hello!`);
-//    }
-//  }
-
-// newObject.username = 'Aja'; // reassign value 
-// newObject.sayHello(); // how to call a function in an object 
-
-// iterate over an object
-
-// for in loop
-
-// const userTwo = {
-//   username: 'trinity.christiana',
-//   password: 'xyz0987',
-//   lovesJavascript: true,
-//   favoriteNumber: 12,
-// }
-
-// for (let key in userTwo) { // gives keys inside object 
-//   console.log(key);
-// }
-
-// for (let key in userTwo) {
-//   console.log(userTwo[key]);
-// }
-
-// for (let key in userTwo) { // dot notation does not work here
-//   console.log(userTwo.key);
-// }
-// invoke a function with values inside the object
-
-// the DOM -- document object model -- where the structure appears a model of the html css and js -- 
-// doc has methods that we can work on outside of the html css and js it contains 
-// the browser
-// callbacks are calling a function based on what the user is doing or user interaction
-// event listeners are what happens on the dom 
-// bootstrap is a css library allows us to piggyback off of html and css elements that are built in
-// documents is js running on a webpage has access to a global object call document
-// looking at two selectors -- 
-
-
-
-// const div = document.getElementById('divId'); // grabbing from html to js to add functionality 
-// const getVariable = document.getElementById('main-btn');
-// console.log(getVariable);
-
-// document.querySelector
-
-// const div = document.querySelector('#divId'); // classes or ids are targeted here new way of doing get element by id # for id and . for class
-// only returns the first on class and should only be one id
-// don't even bother targeting class in js only in css to style multiple elements and id is for js
-
-// const newVariable = document.querySelector('#main-btn');
-// console.log(newVariable);
-
-// CALLBACKS
-
-// the ability to pass a function as an argument to another function
-// convention is to use the 'cb' argument it is a taco
-
-// function saysHelloToUser(user) {
-//   return `Hello ${user}!`;
-// }
-
-// function saysGoodbyeToUser(user) {
-//   return `Goodbye ${user}!`;
-// }
-
-// function createGreeting(user, cb) {
-//   return cb(user);
-// }
-
-// createGreeting('Dan', saysHelloToUser); // 'Hello Dan!'
-// createGreeeting('Dan', saysGoodbyeToUser); // 'Goodbye Dan!'
-
-// function wordsToSentence(words) {
-//   return `I love words ${words}!`; 
-
-// }
-
-// function wordsToSentenceTwo(words) {
-//   return `I hate words ${words}!`; 
-// }
-
-// function acceptsArguments(words, cb) {
-//  return cb(words);
-// }
-
-// console.log(acceptsArguments('Dan said', wordsToSentence));
-// console.log(acceptsArguments('Dan said', wordsToSentenceTwo)); // why did this log backwards?
-
-// event-listeners
-const button = document.querySelector('#otherbutton'); 
-const button2 = document.querySelector('#main-btn'); // targets element on dom
-const container = document.querySelector("#container");
-// button.addEventListener('click', () => {
-//   console.log('You clicked on me!');
-// });
-
-// button.addEventListener('click', (e) => { // or event
-//   console.log(`You clicked ${e.target.id}!`);
-  // console.log(e); // access value on event w dot notation or just e for event type
-// });
-
-const whatIsTheId = (e) => {
-  console.log(`You clicked ${e.target.id}!`);
-
-  if (e.target.id === 'main-btn') {
-    container.innerHTML = 'You clicked the MAIN BUTTON'; // innerHTML puts this in the div after the event 
-  } else {
-    container.innerHTML = 'You clicked the OTHER BUTTON'; // will replace text that is already in there once event occurs
-  }
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToPrint;
 }
 
-button.addEventListener('click', whatIsTheId);
-button2.addEventListener('click', whatIsTheId);
+const pieBuilder = (taco) => {
+  let domString = '';
+  for (let i = 0; i < taco.length; i++) {
+    domString += `<div class="card my-2" style="width: 18rem;" id=${i}>
+                    <div class="img-container" style="background-image: url('${taco[i].imageUrl}');"></div>
+                    <div class="card-body">
+                      <p class="card-text">${taco[i].name}</p>
+                      <p class="card-text">${taco[i].ingredients}</p>
+                      <p class="card-text">${taco[i].bakeTemp}</p>
+                      <p class="card-text">${taco[i].drinkPairing}</p>
+                      <p class="card-text">${taco[i].iceCream}</p>
+                      <button type="button" class="btn btn-danger" id="${i}">Delete</button>
+                    </div>
+                  </div>`;
+  }
 
-// methods on the DOM
+  printToDom('#pies', domString);
+}
+
+const handleButtonClick = (e) => {
+  const buttonId = e.target.id;
+
+  // CHANGING BG COLOR BASED ON BUTTON CLICK
+  if (buttonId === 'Trinity') {
+    // DARK MODE
+    document.querySelector('body').style.backgroundColor = '#000';
+  } else if (buttonId === 'Doc') {
+    // LIGHT MODE
+    document.querySelector('body').style.backgroundColor = '#FFF';
+  } else if (buttonId === 'Aja') {
+    // MEDIUM MODE
+    document.querySelector('body').style.backgroundColor = '#808080';
+  } else if (buttonId === 'All') {
+    // DEFAULT
+    document.querySelector('body').style.backgroundColor = 'rgb(175, 196, 175)';
+  }
+
+  // UPDATE THE PIES BASED ON BUTTON CLICKED
+  const selectedPies = [];
+  // pies[0].instructor // 'Doc'
+  for (let i = 0; i < pies.length; i++) {
+    if (pies[i].instructor === buttonId) {
+      selectedPies.push(pies[i]);
+    }
+  }
+
+  if (buttonId === 'All') {
+    // PRINT ALL THE PIES
+    pieBuilder(pies);
+  } else {
+    pieBuilder(selectedPies);
+  }
+
+}
+
+const buttonEvents = () => {
+  document.querySelector('#All').addEventListener('click', handleButtonClick);
+  document.querySelector('#Doc').addEventListener('click', handleButtonClick);
+  document.querySelector('#Aja').addEventListener('click', handleButtonClick);
+  document.querySelector('#Trinity').addEventListener('click', handleButtonClick);
+}
+
+const init = () => {
+  buttonEvents();
+  pieBuilder(pies);
+}
+
+init();
